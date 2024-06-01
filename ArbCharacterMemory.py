263a4e6b3b6be49cbfe753f78bbf33db899e9fd3
OLDMAN <sputiv@gmail.com>
2024-06-01 05:10:12 +0300

    Characters Relationships basis

--- ArbCharacterMemory.py
+++ ArbCharacterMemory.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 import datetime
-
+from dataclasses import dataclass
 from ArbDatabase import DataManager
 
+
+class RelationType:
+    def __init__(self, relation_id:str, **kwargs):
+        self.data_manager = kwargs.get('data_manager', DataManager())
+        self.relation_id = relation_id
+        data = self.fetch_relation_role_data()
+        self.label = data.get('label', 'Неивзестное отношение')
+        self.desc = data.get('desc', 'Нет описания отношений')
+        self.trust = data.get('trust', 0)
+        self.sympathy = data.get('sympathy', 0)
+        self.respect = data.get('respect', 0)
+        self.love = data.get('love', 0)
+
+    def fetch_relation_role_data(self):
+        if not self.data_manager.check('RELATION_ROLES', f'id = "{self.relation_id}"'):
+            return {}
+        else:
+            return self.data_manager.select_dict('RELATION_ROLES', filter=f'id = "{self.relation_id}"')[0]
+
+    def __repr__(self):
+        return f'RelationType.{self.relation_id}'
 
 class MemoryEvent:
     def __init__(self, event_id:str, **kwargs):
@@ -18,6 +39,7 @@ class MemoryEvent:
         self.trust = data.get('trust', 0)
         self.sympathy = data.get('sympathy', 0)
         self.respect = data.get('respect', 0)
+        self.love = data.get('love', 0)
 
         self.time_to_forget = data.get('time_to_forget', 0)
         self.description = data.get('desc', 'Неизвестное событие произошло...')
@@ -52,7 +74,7 @@ class CharacterMemoryEvent(MemoryEvent):
     def forget(self):
         self.data_manager.delete('CHARS_MEMORY', f'event_id = {self.memory_id}')
 
-    def check_if_forgotten(self):
+    def check_if_timeout(self):
         now = datetime.datetime.today()
         then = datetime.datetime.strptime(self.date, '%Y-%m-%d')
 
@@ -67,8 +89,8 @@ class CharacterMemoryEvent(MemoryEvent):
         else:
             return False, delta
 
-    def forget_if_forgotten(self):
-        is_forgotten, time = self.check_if_forgotten()
+    def forget_if_timeout(self):
+        is_forgotten, time = self.check_if_timeout()
 
         if is_forgotten:
             self.forget()
@@ -76,4 +98,81 @@ class CharacterMemoryEvent(MemoryEvent):
         else:
             return False, time
 
-print(CharacterMemoryEvent(1).forget_if_forgotten())
+
+@dataclass()
+class Relation:
+    actor_id: int
+    subject_id: int
+    relation_type: RelationType
+    family_relation_type: RelationType | None
+    trust: int
+    sympathy: int
+    respect: int
+    love:int
+
+class CharacterRelations:
+    def __init__(self, character_id:int, **kwargs):
+        self.data_manager = kwargs.get('data_manager', DataManager())
+        self.id = character_id
+
+        self.relation_to_character = self.fetch_relation_to_character()
+        self.relation_of_character = self.fetch_character_relation()
+        self.relationships = self.get_character_relationships()
+    def get_character_relationships(self):
+        of_character = self.fetch_character_relation()
+        to_character = self.fetch_relation_to_character()
+
+        total_relationships = {}
+        for i in of_character.keys():
+            total_relationships[i] = [of_character[i], None]
+
+        for i in to_character.keys():
+            if i not in of_character:
+                total_relationships[i] = [None, to_character[i]]
+            else:
+                total_relationships[i][1] = to_character[i]
+
+        return total_relationships
+
+
+    def fetch_character_relation(self) -> dict[Relation]:
+        if self.data_manager.check('CHARS_RELATIONS',f'id = {self.id}') is None:
+            return {}
+
+        relationships = {}
+        process_list = self.data_manager.select_dict('CHARS_RELATIONS',filter=f'id = {self.id}')
+        for rel in process_list:
+            c_rel = Relation(actor_id=self.id,
+                             subject_id=rel.get('subject_id', None),
+                             relation_type=RelationType(rel.get('relation_id', 'Unknown'), data_manager=self.data_manager) if rel.get('relation_id', None) else None,
+                             family_relation_type=RelationType(rel.get('family_id', None), data_manager=self.data_manager) if rel.get('family_id', None) else None,
+                             trust=rel.get('trust', 0) if rel.get('trust', None) else 0,
+                             sympathy=rel.get('sympathy', 0) if rel.get('sympathy', None) else 0,
+                             respect=rel.get('respect', 0) if rel.get('respect', None) else 0,
+                             love=rel.get('love', 0) if rel.get('love', None) else 0)
+
+            relationships[rel.get('subject_id', None)] = c_rel
+
+        return relationships
+
+    def fetch_relation_to_character(self):
+        if self.data_manager.check('CHARS_RELATIONS', f'subject_id = {self.id}') is None:
+            return {}
+
+        relationships = {}
+        process_list = self.data_manager.select_dict('CHARS_RELATIONS', filter=f'subject_id = {self.id}')
+        for rel in process_list:
+            c_rel = Relation(actor_id=rel.get('id', None),
+                             subject_id=self.id,
+                             relation_type=RelationType(rel.get('relation_id', 'Unknown'), data_manager=self.data_manager) if rel.get('relation_id', None) else None,
+                             family_relation_type=RelationType(rel.get('family_id', None), data_manager=self.data_manager) if rel.get('family_id', None) else None,
+                             trust=rel.get('trust', 0) if rel.get('trust', None) else 0,
+                             sympathy=rel.get('sympathy', 0) if rel.get('sympathy', None) else 0,
+                             respect=rel.get('respect', 0) if rel.get('respect', None) else 0,
+                             love=rel.get('love', 0) if rel.get('love', None) else 0)
+
+            relationships[rel.get('id', None)] = c_rel
+
+        return relationships
+
+print(CharacterRelations(0).get_character_relationships())
